506b462afa8d91e1bfebef2c98164193e20e12a5
aemejuobi <govallen@gmail.com>
2018-12-16 21:23:12 -0600

    Trivia Game

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+var me = $("#start").on("click", function(){
+    console.log("yo");
+});
+
+// var start = document.getElementById("start");
+// console.log(start);
+// start.addEventListener("click", function(){
+//     document.getElementById("heading").innerHTML = "Chicken";
+// });
